3f1cec3ba1ba1f6ee1fe1158a5ec4fc93f543cbd
ODinchev <oleg_dinchev@abv.bg>
2018-11-05 08:27:33 -0800

    bug fixes in main class

--- IOT/main.cpp
+++ IOT/main.cpp
@@ -1,20 +1,28 @@
-/*
- * main.cpp
- *
- *  Created on: Nov 5, 2018
- *      Author: pc
- */
-
 #include <iostream>
+#include <uninstd.h>
+#include <pthread.h>
 #include "HIH8120.h"
+using namespace std;
 namespace exploringBB
 {
 
 int main(int argc , char* argv[])
 {
 	unsigned int i2c_bus = 2 ;
+	if(argc > 1)
+	{
+		if(*argv[1] >= '0' && *argv[1] <= '2')
+		{
+			i2c_bus = *argv[1] - 0x30;
+		}
+		else
+		{
+			cout << "Usage:"<< argv[0] << "[i2c_bus] \r\n";
+			cout << "i2c-bus is either 0,1 or 2. Default is 2 \r\n";
+			return 1;
+		}
+	}
 	HIH8120 sensor(i2c_bus,0x27);
-	// display values
 	sensor.displayData();
 	return 0;
 
